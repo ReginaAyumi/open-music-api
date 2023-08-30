f955dd8b5d7332236baa0d5ec1950c8444e81cf2
ReginaAyumi <reginaayumi@students.unnes.ac.id>
2023-08-30 13:03:59 +0700

    add foregin key table songs to albums.id

--- migrations/1692681875244_create-table-songs.js
+++ migrations/1692681875244_create-table-songs.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable camelcase */
 exports.shorthands = undefined;
 
@@ -30,8 +31,13 @@ exports.up = (pgm) => {
       type: 'TEXT',
     },
   });
+
+  // memberikan constraint foreign key pada kolom album_id terhadap albums.id
+  pgm.addConstraint('songs', 'fk_songs.album_id_albums.id', 'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
   pgm.dropTable('songs');
+
+  pgm.dropConstraint('songs', 'fk_songs.album_id_albums.id');
 };
